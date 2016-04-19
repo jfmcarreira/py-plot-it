a645f2ebff421fa49bef227cdd3742a2bb44babb
Joao Carreira <jfmcarreira@gmail.com>
2016-04-19 16:32:23 +0100

    Remove second cfgData example

--- plotGenerator/cfgData.py
+++ plotGenerator/cfgData.py
@@ -93,7 +93,8 @@ for i in range( len( ConfigsCond.configs ) ):
 XValues = [(6, "Bitrate [kbps]"), (7, "Packet Loss Ratio [%]") ]
 YValues = [(7, "PSNR [dB]") ]
 
-
+#AxisLimitDefaultX = "0, 5, 100"
+#AxisLimitDefaultY = "30, 1, 42"
 
 ###################################################################################################################
 # Gnuplot template

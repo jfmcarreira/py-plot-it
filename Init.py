41c657aa5cb2a46f434c7c60a5a778d4a7efc5fd
Joao Carreira <jfmcarreira@gmail.com>
2020-05-11 00:42:23 +0100

    Add missing import

--- Init.py
+++ Init.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import os, sys
+from operator import itemgetter
 from MiscFct import *
 
 ############################################################################################
